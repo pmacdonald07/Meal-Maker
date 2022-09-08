4960933b0ddf7a335d0705774d9839f4fce6b137
Mitchell Armstrong <mra0211@gmail.com>
2022-09-08 18:13:39 -0400

    Added an array of different apiKeys to be randomly selected to be used in the fetch api call. Changed the displayRecipeCards function a little bit to have the cards display the corresponding image and name of the recipe with the user's input.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
-var userInput = document.querySelector("#user-form");
-var submitButton = document.querySelector(".submit");
+var userInput = $("#user-form");
+var submitButton = $("#form-submit");
 
-var patrickApiKey = "d4e89512419b4ecfae9d762561d78c97";
+// An array of different apiKeys that will work in the fetch api call in the getSpoonApi function
+var arrApiKeys = [
+  "d4e89512419b4ecfae9d762561d78c97",
+  "c39f000be15b48f0b51fc4215771d97b",
+];
+
 var cardContainerEl = $("#cards");
+
 var getSpoonApi = function (event) {
   event.preventDefault();
   const options = {
@@ -13,16 +19,21 @@ var getSpoonApi = function (event) {
     },
   };
 
+  // Converts the user's input into a value the apiUrl will be able to read
   var userText = document.querySelector(".input");
   var input = userText.value.trim();
 
+  // Chooses an apiKey at random from the arrApiKeys to be used in the fetch api call
+  function randomKey(arrApiKeys) {
+    return arrApiKeys[Math.floor(Math.random() * arrApiKeys.length)];
+  }
+
   console.log(input);
-  var apiKey = "c39f000be15b48f0b51fc4215771d97b";
   var apiUrl =
     "https://api.spoonacular.com/food/search?query=" +
     input +
-    "&number=10&apiKey=" +
-    apiKey;
+    "&number=5&apiKey=" +
+    randomKey(arrApiKeys);
 
   fetch(apiUrl)
     .then(function (response) {
@@ -42,6 +53,11 @@ var getSpoonApi = function (event) {
 
 // this function needs to have response from the API call as a parameter
 var displayRecipeCards = function (data) {
+  var boxContainerEl = $("#box");
+  var resultsTextHeader = $("<h1></h1>");
+  resultsTextHeader = "Here are the top 5 recipes for:  '" + data.query + "' ";
+  boxContainerEl.append(resultsTextHeader);
+
   // create row for forecast cards
   recipeContainerEl = $("<div></div");
   recipeContainerEl.attr("class", "columns");
@@ -63,7 +79,7 @@ var displayRecipeCards = function (data) {
     cardImg.append(cardFigureEl);
 
     cardImgEl = $("<img></img>");
-    cardImgEl.attr("src", data.results[i].image);
+    cardImgEl.attr("src", data.searchResults[0].results[i].image);
     cardImgEl.attr("alt", "Picture of recipe");
     cardFigureEl.append(cardImgEl);
 
@@ -75,7 +91,7 @@ var displayRecipeCards = function (data) {
     // set card title
     cardTitleEl = $("<p></p>");
     cardTitleEl.attr("class", "title is-4");
-    cardTitleEl.text(data.results[i].title);
+    cardTitleEl.text(data.searchResults[0].results[i].name);
     cardBodyEl.append(cardTitleEl);
 
     // set card content
